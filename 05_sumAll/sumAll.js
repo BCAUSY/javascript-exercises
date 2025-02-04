98edea6d816620cfe620627094e08b92823e745a
bacusy <191461931+BCAUSY@users.noreply.github.com>
2025-02-04 01:03:08 +0100

    05_sumAll bigger number first to do

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum,lastNum) {
+    let finalSum = 0;
+    let iteration = lastNum - firstNum;
+    if (firstNum > 0 && lastNum > 0 && firstNum - Math.trunc(firstNum) === 0 && lastNum - Math.trunc(lastNum) === 0 && typeof firstNum === 'number' && typeof lastNum === 'number') {
+        for (let i = firstNum; i <= lastNum; i++) { 
+            finalSum += i;
+        }
+        return finalSum 
+} else {
+    return "ERROR"
+}
 };
 
 // Do not edit below this line
